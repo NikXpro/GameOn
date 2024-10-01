6ab834e33fbfc4f1e3c28141272e84414a979c7d
NikX <41104633+NikXpro@users.noreply.github.com>
2024-10-01 15:46:31 +0200

    feat(modal.js): implement form validation for first name, last name, email,   quantity, location selection, and terms acceptance to enhance user input   reliability and improve user experience.

--- modal.js
+++ modal.js
@@ -34,6 +34,84 @@ function closeModal() {
 function validate() {
   let isValid = true;
 
+  // First name validation
+  const firstName = document.getElementById("first");
+  if (firstName.value.trim().length < 2) {
+    isValid = false;
+    displayError(
+      firstName,
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
+  } else {
+    hideError(firstName);
+  }
+
+  // Last name validation
+  const lastName = document.getElementById("last");
+  if (lastName.value.trim().length < 2) {
+    isValid = false;
+    displayError(
+      lastName,
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    );
+  } else {
+    hideError(lastName);
+  }
+
+  // Email validation
+  const email = document.getElementById("email");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email.value)) {
+    isValid = false;
+    displayError(email, "Veuillez entrer une adresse email valide.");
+  } else {
+    hideError(email);
+  }
+
+  // Quantity validation
+  const quantity = document.getElementById("quantity");
+  if (quantity.value === "" || isNaN(quantity.value) || quantity.value < 0) {
+    isValid = false;
+    displayError(quantity, "Veuillez entrer un nombre valide.");
+  } else {
+    hideError(quantity);
+  }
+
+  // Location validation
+  const locations = document.querySelectorAll('input[name="location"]');
+  let locationChecked = false;
+  locations.forEach((location) => {
+    if (location.checked) {
+      locationChecked = true;
+    }
+  });
+  if (!locationChecked) {
+    isValid = false;
+    displayError(locations[0], "Vous devez choisir une option.");
+  } else {
+    hideError(locations[0]);
+  }
+
+  // Terms validation
+  const terms = document.getElementById("checkbox1");
+  if (!terms.checked) {
+    isValid = false;
+    displayError(
+      terms,
+      "Vous devez vérifier que vous acceptez les termes et conditions."
+    );
+  } else {
+    hideError(terms);
+  }
+
+  if (isValid) {
+    closeModal();
+    alert("Merci ! Votre réservation a été reçue.");
+  }
+
+  return isValid;
+}
+
 function displayError(element, message) {
   let errorDiv = element.nextElementSibling;
   if (!errorDiv || !errorDiv.classList.contains("error-message")) {
